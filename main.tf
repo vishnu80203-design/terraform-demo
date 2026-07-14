d13e92482393bc020c4588c89c4f2726b1132dda
vishnu80203-design <vishnu80203@gmail.com>
2026-07-14 08:12:25 +0000

    Add virtual network and storage account resources
    Added a new virtual network and a new storage account to the existing resource group.

--- main.tf
+++ main.tf
@@ -1,4 +1,23 @@
+# Existing Resource Group
 resource "azurerm_resource_group" "jenkins_rg" {
   name     = "myJenkinsResourceGroup"
   location = "eastus"
+}
+
+# New Virtual Network
+resource "azurerm_virtual_network" "jenkins_vnet" {
+  name                = "jenkins-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.jenkins_rg.location
+  resource_group_name = azurerm_resource_group.jenkins_rg.name
+}
+
+# New Storage Account
+resource "azurerm_storage_account" "jenkins_storage" {
+  # MUST BE GLOBALLY UNIQUE. Change "vishnujenkinsstore80203" if it fails.
+  name                     = "vishnujenkinsstore80203" 
+  resource_group_name      = azurerm_resource_group.jenkins_rg.name
+  location                 = azurerm_resource_group.jenkins_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
