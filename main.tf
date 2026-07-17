24fdc959688604707e2d03263d20456b0bebfa8d
vishnu80203-design <vishnu80203@gmail.com>
2026-07-17 07:15:16 +0000

    Migrate from Azure resources to AWS VPC
    Removed Azure resources and added AWS VPC configuration.

--- main.tf
+++ main.tf
@@ -1,23 +1,9 @@
-# Existing Resource Group
-resource "azurerm_resource_group" "jenkins_rg" {
-  name     = "myJenkinsResourceGroup"
-  location = "eastus"
-}
+resource "aws_vpc" "jenkins_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
-# New Virtual Network
-resource "azurerm_virtual_network" "jenkins_vnet" {
-  name                = "jenkins-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.jenkins_rg.location
-  resource_group_name = azurerm_resource_group.jenkins_rg.name
-}
-
-# New Storage Account
-resource "azurerm_storage_account" "jenkins_storage" {
-  # MUST BE GLOBALLY UNIQUE. Change "vishnujenkinsstore80203" if it fails.
-  name                     = "vishnujenkinsstore80203" 
-  resource_group_name      = azurerm_resource_group.jenkins_rg.name
-  location                 = azurerm_resource_group.jenkins_rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+  tags = {
+    Name = "Jenkins-AWS-Test-VPC"
+  }
 }
