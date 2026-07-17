8a39d042dc38d555937e1625e5e126b099298519
vishnu80203-design <vishnu80203@gmail.com>
2026-07-17 07:13:18 +0000

    Switch from AzureRM to AWS provider
    Replaced Azure provider configuration with AWS provider configuration.

--- provider.tf
+++ provider.tf
@@ -1,12 +1,12 @@
 terraform {
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
 
-provider "azurerm" {
-  features {}
+provider "aws" {
+  region = "us-east-1" # Change this if you want a different AWS region
 }
