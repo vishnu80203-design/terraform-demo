1e1e4284ed41f32fbd230472370840c6d70c6805
vishnu80203-design <vishnu80203@gmail.com>
2026-07-17 07:12:11 +0000

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "terraform-state-rg"
-    storage_account_name = "vishnutstate80203"  # Must match what you created
-    container_name       = "tfstate"
-    key                  = "prod.terraform.tfstate" # The name of the state file it will create
-  }
-}
